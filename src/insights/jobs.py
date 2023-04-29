aaea28ff998ce3eb0d6328b1b1d0efef6c90c0b1
IsraelPereira <pereira.israel2070@gmail.com>
2023-04-29 08:38:19 -0300

    feat: implementando a função de listagem dos tipos de trabalho

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -22,20 +22,17 @@ def read(path: str) -> List[Dict]:
 
 def get_unique_job_types(path: str) -> List[str]:
     """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
     """
-    raise NotImplementedError
+    unique_job_types = []
+    try:
+        job_list = read(path)
+        for job in job_list:
+            if job['job_type'] not in unique_job_types:
+                unique_job_types.append(job['job_type'])
+    except FileNotFoundError:
+        raise FileNotFoundError("Arquivo não encontrado: " + path)
+    else:
+        return unique_job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:

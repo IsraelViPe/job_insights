5d16a4148f7df950a661e17a4cfa10428c9181e9
IsraelPereira <pereira.israel2070@gmail.com>
2023-04-30 08:05:15 -0300

    feat: implementa a lista de tipos de industrias

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,23 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    """Checks all different industries and returns a list of them"""
+    unique_industries = []
+    try:
+        jobs = read(path)
+        for job in jobs:
+            if job['industry'] and job['industry'] not in unique_industries:
+                unique_industries.append(job['industry'])
+    except KeyError:
+        print(f"Chave inexistente em {job}: {job['industry']}")
+    else:
+        print(unique_industries)
+        return unique_industries
+    
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+get_unique_industries("data/jobs.csv")
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,8 +5,7 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-    """
+    """Reads a file from a given path and returns its contents"""
     result = []
     try:
         with open(path, encoding="utf-8") as file:
@@ -21,16 +20,15 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-    """
+    """Checks all different job types and returns a list of them"""
     unique_job_types = []
     try:
         job_list = read(path)
         for job in job_list:
             if job['job_type'] not in unique_job_types:
                 unique_job_types.append(job['job_type'])
-    except FileNotFoundError:
-        raise FileNotFoundError("Arquivo não encontrado: " + path)
+    except KeyError:
+        print(f"Chave inexistente em {job}: {job['job_type']}")
     else:
         return unique_job_types
 

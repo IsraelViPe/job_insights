bed01147e0d30b5cfaf7199a5c7317051df2cd95
IsraelPereira <pereira.israel2070@gmail.com>
2023-05-06 17:13:52 -0300

    feat: implementa teste da função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    """Conta a ocorrência da palavra 'back' no arquivo mockado"""
+    assert count_ocurrences('tests/mocks/jobs.csv', 'Back') == 1
+    assert count_ocurrences('tests/mocks/jobs.csv', 'back') == 1
